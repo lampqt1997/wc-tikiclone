30adeb98f7059cd9638a51f685e2f1f8883dc27a
dangsg <lyhoangdang96@gmail.com>
2020-08-24 21:48:03 +0700

    Add javascript file to load HTML header

---
+++ js/header.js
@@ -0,0 +1,74 @@
+document.write(`<header class="header bg-primary">
+    <div class="container px-0">
+      <nav class="navbar navbar-expand-lg navbar-light bg-primary px-0">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a class="navbar-brand text-white h1" href="#"><strong>Tiki</strong></a>
+          <img src="media/img/logo.png" alt="" class="img-fluid" style="max-width: 80px;">
+          <div class="input-group col-6">
+            <input type="text" class="form-control" placeholder="Tìm sản phẩm, danh mục hay thương hiệu mong muốn ...">
+            <div class="input-group-append">
+              <span class="input-group-text" id="basic-addon2">Tìm kiếm</span>
+            </div>
+          </div>
+          <ul class="navbar-nav col-5 px-0">
+            <li class="nav-item active">
+              <i class="fas fa-archive text-white"></i>
+              <span class="nav-link text-white d-inline" href="#">Theo dõi đơn hàng </span>
+            </li>
+            <li class="nav-item">
+                <i class="fas fa-bell text-white"></i>
+              <span class="nav-link text-white d-inline" href="#">Thông báo của tôi</span>
+            </li>
+
+            <li class="nav-item">
+                <i class="fas fa-user text-white"></i>
+              <span class="nav-link text-white d-inline" href="#">Đăng nhập tài khoản</span>
+            </li>
+            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">
+              <i class="fas fa-shopping-cart"></i>
+              Giỏ hàng
+            </button>
+
+          </ul>
+
+        </div>
+      </nav>
+      <nav class="navbar navbar-expand-lg navbar-light bg-primary px-0">
+        <a class="navbar-brand text-white" href="#">Danh mục sản phẩm</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
+          aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link text-white" href="#"> </i> Địa chỉ </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Sản phẩm bạn đã xem</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Giao hàng nhanh</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Thực phẩm tươi</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Sản phẩm chính hiệu</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">30 ngày đổi trả </a>
+            </li>
+
+          </ul>
+
+        </div>
+      </nav>
+
+    </div>
+  </header>`);
