543db59dc9c118e779386ec0dd61e722d7771c19
dangsg <lyhoangdang96@gmail.com>
2020-08-24 21:48:19 +0700

    Add javascript file to load HTML footer

---
+++ js/footer.js
@@ -0,0 +1,102 @@
+document.write(`<footer class="footer mt-5  ">
+    <div class="container px-0">
+      <div class="row px-0  ">
+        <div class="col-2 px-0">
+          <img src="media/footer/newsletter.png" alt="" class="img-fluid">
+        </div>
+        <div class="col-5 row align-content-end">
+          <span class="text-secondary d-block h5">Đăng ký nhận bản tin Tiki </span>
+          <span class="text-secondary d-block h6">Đừng bỏ lỡ hàng ngàn sản phẩm và chương trình siêu hấp dẫn </span>
+        </div>
+        <div class="col-5 row align-content-end ml-auto">
+          <form class="form-inline w-100">
+            <input class="form-control col-9" type="text" placeholder="Địa chỉ email của bạn" aria-label="emailFooter">
+            <button class="btn btn-info my-2 my-sm-0 col-3" type="submit">
+              <i class="fas fa-search"></i>
+              Tìm kiếm
+            </button>
+          </form>
+        </div>
+      </div>
+     
+     
+    </div>
+    <div class="footer-content bg-white mt-5 border-bottom">
+        <div class="container">
+          <div class="row py-5">
+            <div class="col-3">
+              <div class="footer-sub-title">Hỗ trợ khách hàng</div>
+              <small class="text-danger">Hotline chăm sóc khách hàng: 1900-6035</small>
+              <small> (1000đ/phút , 8-21h kể cả T7, CN)</small>
+
+              <small> Các câu hỏi thường gặp</small>
+              <small> Gửi yêu cầu hỗ trợ</small>
+              <small> Hướng dẫn đặt hàng</small>
+              <small>Phương thức vận chuyển</small>
+              <small> Chính sách đổi trả</small>
+              <small> Hướng dẫn mua trả góp</small>
+              <small> Chính sách hàng nhập khẩu</small>
+              <small> Hỗ trợ khách hàng: hotro@tiki.vn</small>
+              <small>Báo lỗi bảo mật: security@tiki.vn  </small>
+            </div>
+            <div class="col">
+                <div class="footer-sub-title"> VỀ TIKI</div>
+                <small>Giới thiệu Tiki</small>
+                <small> Tuyển Dụng</small>
+  
+                <small>  Chính sách bảo mật thanh toán</small>
+                <small> Chính sách bảo mật thông tin cá nhân</small>
+                <small> Hướng dẫn đặt hàng</small>
+                <small> Chính sách giải quyết khiếu nại</small>
+                <small>  Điều khoản sử dụng</small>
+                <small>  Giới thiệu Tiki Xu</small>
+                <small>  Bán hàng doanh nghiệp</small>
+                <small>   HỢP TÁC VÀ LIÊN KẾT</small>
+                <small>Quy chế hoạt động Sàn GDTMĐT</small>
+                <small>  Bán hàng cùng Tiki</small>
+            </div>
+            <div class="col">
+                <div class="footer-sub-title"> HỢP TÁC VÀ LIÊN KẾT </div>
+                <small>Quy chế hoạt động Sàn GDTMĐT</small>
+                <small> Bán hàng cùng Tiki</small>
+            </div>
+            <div class="col"> 
+                <div class="footer-sub-title"> PHƯƠNG THỨC THANH TOÁN </div>
+                <div class="row">
+                  <div class="col-4 mx-0 mt-2"><img src="media/footer/visa.svg" alt=""></div>
+                  <div class="col-4 mx-0 mt-2"><img src="media/footer/mastercard.svg" alt=""></div>
+                  <div class="col-4 mx-0 mt-2"><img src="media/footer/jcb.svg" alt=""></div>
+                  <div class="col-4 mx-0 mt-2"><img src="media/footer/cash.svg" alt=""></div>
+                  <div class="col-4 mx-0 mt-2"><img src="media/footer/internet-banking.svg" alt=""></div>
+                  <div class="col-4 mx-0 mt-2"><img src="media/footer/installment.svg" alt=""></div>
+                 
+                </div>
+            </div>
+            <div class="col">
+              <div class="footer-sub-title">KẾT NỐI VỚI CHÚNG TÔI</div>
+              <span><i class="fab fa-facebook-f text-info"></i></span>
+              <span><i class="fab fa-youtube text-danger"></i></span>
+              <span class="text-info h6"> Zalo</span>
+
+              <div class="footer-sub-title mt-5">KẾT NỐI VỚI CHÚNG TÔI</div>
+              <div class=" col-9 pl-0"><img src="media/footer/appstore.png" alt="" class="img-fluid"></div>
+              <div class="col-9 pl-0"><img src="media/footer/playstore.png" alt="" class="img-fluid"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer-end bg-white pt-5 pb-5">
+        <div class="container">
+          <p class="offset-1 col-6"><Strong>Địa chỉ văn phòng:</Strong> <small>52 Út Tịch, phường 4, quận Tân Bình, thành phố Hồ Chí Minh</small> 
+          <p class="offset-1 col-6 mb-5"><small>Tiki nhận đặt hàng trực tuyến và giao hàng tận nơi, chưa hỗ trợ mua và nhận hàng trực tiếp tại văn phòng hoặc trung tâm xử lý đơn hàng</small></p>
+          <p class="offset-0 col-5 mt-5 mb-0"><strong>© 2016 - Bản quyền của Công Ty Cổ Phần Ti Ki - Tiki.vn </strong>         </p>
+          <span class="offset-0 col-5 mt-0"><small>Giấy chứng nhận Đăng ký Kinh doanh số 0309532909 do Sở Kế hoạch và Đầu tư Thành phố Hồ Chí Minh cấp ngày 06/01/2010</small> </span>
+          
+          <div class="col-1 d-inline-block mr-0 float-right pr-0"><img src="media/footer/bo-cong-thuong.svg" alt="" ></div>
+          <div class="col-1 d-inline-block float-right ml-0" ><img src="media/footer/bo-cong-thuong-2.png" alt="" style="width: 46px; height: 46px"></div>
+          
+         
+        </div>
+          
+        </div>
+  </footer>`)
