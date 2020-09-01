f6ca1e995e693543efec7604e537a660f89d9ec8
Lam Martin <thachlam1997@gmail.com>
2020-09-01 18:26:46 +0700

    Bo sung file js, css - Giao dien trang index - issue 2409 - lampqt

--- js/index.js
+++ js/index.js
@@ -1,18 +1,33 @@
 $(document).ready(function() {
   var danhmucs = dataMenu();
   var es = fillDataMenu();
-  var deals = dataDealHot();
-  var deal_hot = fillDataDealHot(deals);
-  var inds = dataIntrustry();
-  var industry = fillDataIntrustry(inds);
-  var key = dataKeyHot();
-  var keyhot = fillDataKeyHot(key);
   
+  var datadeals = dataDealHot();
+  var deal_hot = fillDataDealHot(datadeals);
+  
+  var datainds = dataIntrustry();
+  var industry = fillDataIntrustry(datainds);
+ 
+  var datakey = dataKeyHot();
+  var keyhot = fillDataKeyHot(datakey);
+
+  var dataproductbox = dataProductBox();
+  var productBox = fillDataProductBox(dataproductbox);
+
+  // console.log(found);
+  
+  $('.btn-cart-click').click(function (e) { 
+    e.preventDefault();
+    window.location.href  = 'cart.html';
+  });
+
   $("#menu-nav").append(es);
   $('.deal-hot-append').append(deal_hot);
   $('.intrustry-append').append(industry);
   $('.keyhot-append').append(keyhot);
-  
+  $('.product-box-append').append(productBox);
+
+  //hover menu
   $("li.nav-item.nav-item-custom").hover(
     function() {
       let code = $(this).data("code");
@@ -35,4 +50,26 @@ $(document).ready(function() {
     $(".menu-hover").removeClass("d-block");
 
   });
+
+  
+
 });
+window.onload = function () {
+  let item = sessionStorage.getItem("username");
+  console.log(item);
+  if (item !== null) {
+    document.getElementById("labelLginModal").innerText = item;
+  }
+};
+
+function login() {
+  sessionStorage.setItem("prevPage", window.location.href);
+  let item = sessionStorage.getItem("username");
+  if (item !== null) {
+    sessionStorage.removeItem("username");
+    console.log("logout"+ sessionStorage.removeItem("username"));
+    document.getElementById("labelLginModal").innerText = "Đăng nhập tài khoản";
+  } else {
+    location.replace("login.html");
+  }
+}
